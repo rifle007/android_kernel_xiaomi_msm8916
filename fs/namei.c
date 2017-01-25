7768704db6f03b53346a6bd09059f753d7b02251
Tom Marshall <tdm.code@gmail.com>
2017-01-25 18:01:03 +0100

    kernel: Only expose su when daemon is running
    It has been claimed that the PG implementation of 'su' has security
    vulnerabilities even when disabled.  Unfortunately, the people that
    find these vulnerabilities often like to keep them private so they
    can profit from exploits while leaving users exposed to malicious
    hackers.
    
    In order to reduce the attack surface for vulnerabilites, it is
    therefore necessary to make 'su' completely inaccessible when it
    is not in use (except by the root and system users).
    
    Change-Id: I79716c72f74d0b7af34ec3a8054896c6559a181d

--- fs/namei.c
+++ fs/namei.c
@@ -2025,7 +2025,18 @@ static int path_lookupat(int dfd, const char *name,
 		}
 	}
 
+<<<<<<< HEAD
 out:
+=======
+	if (!err) {
+		struct super_block *sb = nd->inode->i_sb;
+		if (sb->s_flags & MS_RDONLY) {
+			if (d_is_su(nd->path.dentry) && !su_visible())
+				err = -ENOENT;
+		}
+	}
+
+>>>>>>> 9b0b1b1... kernel: Only expose su when daemon is running
 	if (base)
 		fput(base);
 
