cd6d8da50e8d497536697ea2f3077373c931c32a
nulldash <nulldash@yandex.ru>
2017-02-11 21:57:08 +0800

    sched: Added userspace sysfs interface for ARCH_POWER and GENTLE_FAIR_SLEEPERS
    Signed-off-by: rifle007 <mrifai025@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3132,6 +3132,21 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+#ifdef CONFIG_SCHED_FEATURES_SYSFS
+static unsigned int Lgentle_fair_sleepers = 0;
+static unsigned int Larch_power = 1;
+
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
+void relay_ap(unsigned int ap)
+{
+	Larch_power = ap;
+}
+#endif
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -3154,7 +3169,11 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
+#ifdef CONFIG_SCHED_FEATURES_SYSFS
+		if (Lgentle_fair_sleepers)
+#else
 		if (sched_feat(GENTLE_FAIR_SLEEPERS))
+#endif
 			thresh >>= 1;
 
 		vruntime -= thresh;
@@ -5916,7 +5935,11 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	struct sched_group *sdg = sd->groups;
 
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
+#ifdef CONFIG_SCHED_FEATURES_SYSFS
+		if (Larch_power)
+#else
 		if (sched_feat(ARCH_POWER))
+#endif
 			power *= arch_scale_smt_power(sd, cpu);
 		else
 			power *= default_scale_smt_power(sd, cpu);
@@ -5926,7 +5949,11 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 
 	sdg->sgp->power_orig = power;
 
+#ifdef CONFIG_SCHED_FEATURES_SYSFS
+	if (Larch_power)
+#else
 	if (sched_feat(ARCH_POWER))
+#endif
 		power *= arch_scale_freq_power(sd, cpu);
 	else
 		power *= default_scale_freq_power(sd, cpu);
