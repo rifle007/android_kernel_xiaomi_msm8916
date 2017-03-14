72857d47ace7d52e9ef8a862367eb7811ed1cdc8
rifle007 <mrifai025@gmail.com>
2017-03-14 20:21:21 +0700

    cpu freq: smartmax_EPS
    Signed-off-by: rifle007 <mrifai025@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -439,6 +439,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
+extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartmax_eps)
 #endif
 
 /*********************************************************************
